8fd40f0e4afd311b5a2eef645e0e72e552d84ded
Reagan Ochola <111653584+Ocholar@users.noreply.github.com>
2025-07-11 14:23:08 +0300

    Create estimated_yield.sql

---
+++ queries/estimated_yield.sql
@@ -0,0 +1,29 @@
+-- Estimated Yield Calculation
+-- Calculates box areas, average sample yield per m², scales to full plot size
+
+SELECT 
+  Year,
+  Season,
+  Country,
+  PlotSize_acres,
+  
+  BoxAWidth * BoxALength AS BoxAArea,
+  BoxBWidth * BoxBLength AS BoxBArea,
+
+  BoxADryWeight / (BoxAWidth * BoxALength) AS YieldBoxA_per_m2,
+  BoxBDryWeight / (BoxBWidth * BoxBLength) AS YieldBoxB_per_m2,
+
+  ((BoxADryWeight / (BoxAWidth * BoxALength)) + (BoxBDryWeight / (BoxBWidth * BoxBLength))) / 2 AS AvgSampleYield_per_m2,
+
+  (( 
+    (BoxADryWeight / (BoxAWidth * BoxALength)) + 
+    (BoxBDryWeight / (BoxBWidth * BoxBLength)) 
+  ) / 2) * (PlotSize_acres * 4046.86) AS EstimatedYieldKG
+
+FROM yield_metadata
+WHERE 
+  BoxAWidth > 0 AND BoxALength > 0 AND 
+  BoxBWidth > 0 AND BoxBLength > 0 AND 
+  BoxADryWeight IS NOT NULL AND 
+  BoxBDryWeight IS NOT NULL
+ORDER BY EstimatedYieldKG DESC;
